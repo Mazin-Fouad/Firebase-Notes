896f3a4b796df0ee374017cbc09c58b99f1f75af
Mazin Fouad <m.fouad@live.de>
2023-01-30 17:55:09 +0100

    add data functin to collection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  collection,
+  collectionData,
+  doc,
+  Firestore,
+  setDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Firebase-Note';
+  item$: Observable<any>;
+  notes: Array<any>;
+  newTitle: string = '';
+  newText: string = '';
+
+  constructor(private firestore: Firestore) {
+    const coll = collection(firestore, 'notes');
+    this.item$ = collectionData(coll);
+
+    this.item$.subscribe((newNotes) => {
+      alert('New notes Added');
+      this.notes = newNotes;
+    });
+  }
+
+  addTodo() {
+    let newNote = {
+      noteTitle: this.newTitle,
+      noteText: this.newText,
+    };
+    const coll = collection(this.firestore, 'notes');
+    setDoc(doc(coll), newNote);
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'mfnote',
+    appId: '1:112779460657:web:ea81fbd1ee92c461395510',
+    storageBucket: 'mfnote.appspot.com',
+    apiKey: 'AIzaSyDntazqon3TNEtXUiMPtK1yGNCV62Rf83U',
+    authDomain: 'mfnote.firebaseapp.com',
+    messagingSenderId: '112779460657',
+  },};
